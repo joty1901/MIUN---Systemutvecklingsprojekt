8850e383ae39b32e8d7a32379b75bcfb23341d0c
Jonathan <joty1901@users.noreply.github.com>
2020-09-03 13:21:40 +0200

    Test

--- MaterMinds/MainWindow.xaml.cs
+++ MaterMinds/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MaterMinds
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name = "Ludvig";
+        }
     }
 }
