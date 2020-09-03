b3aea8fafb10aa004861a4fdcc2df0436ec9e192
Jonathan <joty1901@users.noreply.github.com>
2020-09-03 13:39:40 +0200

    Skapat "class User" och lagt in properties.

---
+++ MaterMinds/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaterMinds
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Nickname { get; set; }
+
+        public User(int id, string nickname)
+        {
+            Id = id;
+            Nickname = nickname;
+        }
+    }
+}
