ab052d82a5f9f81d0f8db2485c575625caac4991
Ludvig Blixö <ludvigblixo@hotmail.com>
2020-09-08 16:31:35 +0200

    Added Pegs

---
+++ MaterMinds/View/Pegs/MasterPeg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace MaterMinds
+{
+    public class MasterPeg : UserControl
+    {
+        public int ColorIndex { get; set; }
+
+        public MasterPeg()
+        {
+            Height = 30;
+            Name = "Peg";
+            Width = 30;
+        }
+    }
+}
