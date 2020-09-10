2fda0faf49816c086e518dbd08c74df2bd6e3949
Jonathan <joty1901@users.noreply.github.com>
2020-09-10 09:46:18 +0200

    Added interface for Repository called IRepository

---
+++ MaterMinds/Repositories/IRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaterMinds.Repositories
+{
+    interface IRepository
+    {
+        public static void GetDbPlayers()
+        {
+          
+        }
+
+        public static void AddPlayerWithScore(int playerId, int score)
+        {
+
+        }
+
+        public static void GetUserHighscore(Player player)
+        {
+
+        }
+
+        public static void GetTopTenHigscore()
+        {
+            
+        }
+
+        public static void AddPlayer(string nickname)
+        {
+           
+        }
+
+    }
+}
+
