8f1c6e4331711fc98a7c56c4c41dc0994f78f61d
Jonathan <joty1901@users.noreply.github.com>
2020-09-14 14:36:51 +0200

    Test MVVM

---
+++ MaterMinds/Command/UpdateViewCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace MaterMinds.Command
+{
+    public class UpdateViewCommand : ICommand
+    {
+        private MainViewModel viewModel;
+
+        public UpdateViewCommand(MainViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter.ToString() == "Player")
+            {
+                viewModel.SelectedViewModel = new SelectPlayerViewModel();
+            }
+            else if (parameter.ToString() == "Highscore")
+            {
+                viewModel.SelectedViewModel = new HighscoreViewModel();
+            }
+        }
+    }
+}
