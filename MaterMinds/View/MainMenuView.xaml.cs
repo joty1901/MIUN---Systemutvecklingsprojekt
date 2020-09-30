c0a9e58339408cfa5f57ee91544cc25c3bc327fa
Tobias Hall <tobbe085@gmail.com>
2020-09-30 14:35:49 +0200

    -Cleaned ChoosePlayerView.xaml and code behind -Cleaned MainMenuView.xaml and code behind -Cleaned GuessControl.xaml and code behind

--- MaterMinds/View/MainMenuView.xaml.cs
+++ MaterMinds/View/MainMenuView.xaml.cs
@@ -1,23 +1,10 @@
 ﻿using MaterMinds.Model;
 using MaterMinds.ViewModel;
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MaterMinds.View
 {
-    /// <summary>
-    /// Interaction logic for MainMenyView.xaml
-    /// </summary>
     public partial class MainMenuView : UserControl
     {
         private MainMenuViewModel model;
