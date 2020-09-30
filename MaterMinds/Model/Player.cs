afb95066dd1924cbe78b1c5bd43039513c07b80c
Jonathan <joty1901@users.noreply.github.com>
2020-09-30 09:57:25 +0200

    Added System.Diagnostics.PresentationTraceSources.DataBindingSource.Switch.Level to the constructor in GamePage.xaml.cs the data error warning messages in the Output Window

--- MaterMinds/Model/Player.cs
+++ MaterMinds/Model/Player.cs
@@ -11,17 +11,11 @@ namespace MaterMinds
         public string Nickname { get; set; }
         public List<Score> PlayerScore { get; set; }
 
-        /// <summary>
-        /// Constructor that's used when the method GetDbPlayers i executed in Repository.cs
-        /// </summary>
         public Player() 
         {
 
         }
 
-        /// <summary>
-        /// Constructor that's used when a user creates a new player
-        /// </summary>
         public Player(int id, string nickname)
         {
             Id = id;
